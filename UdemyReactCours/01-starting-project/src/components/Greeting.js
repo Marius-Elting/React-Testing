2b9c33d55f346f9209431c00ef09d71f3db47f19
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-12 16:47:20 +0200

    added new tests with first userEvents

--- UdemyReactCours/01-starting-project/src/components/Greeting.js
+++ UdemyReactCours/01-starting-project/src/components/Greeting.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Greeting = () => {
+    const [changedtest, setChangedText] = useState(false)
+
+    const changeTextHandler = () => {
+        setChangedText(true)
+    }
     return (
         <div>
             <h2>Hello World!</h2>
-            <p>Its good to see you</p>
+            {!changedtest && <p>Its good to see you</p>}
+            {changedtest && <p>changed here</p>}
+            <button onClick={changeTextHandler}></button>
         </div>
     )
 }
--- UdemyReactCours/01-starting-project/src/components/Greeting.test.js
+++ UdemyReactCours/01-starting-project/src/components/Greeting.test.js
@@ -1,17 +1,57 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
+import userEvent from "@testing-library/user-event"
+
+describe("Greeting Compontent", () => {
+    test("renders Hello World", () => {
+        // Arrange
+        render(<Greeting />)
+        //Act
+
+        //Assert
+        const helloWorld = screen.getByText(/Hello world/i, { exact: true })
+        //also possible with a string as an Argument const helloWorld = screen.getByText(/Hello world/i)
+        expect(helloWorld).toBeInTheDocument();
+    })
+
+    test("render text doesnt change", () => {
+        render(<Greeting />)
+
+        const text = screen.getByText("Its good to see you")
+        expect(text).toBeInTheDocument()
+    })
 
 
-// AAA
-test("renders Hello World", () => {
-    // Arrange
-    render(<Greeting />)
-    //Act
+    test("render text changes", () => {
+        // Arrange
+        render(<Greeting />)
+        //Act
 
-    //Assert
-    const helloWorld = screen.getByText(/Hello world/i, { exact: true })
-    //also possible with a string as an Argument const helloWorld = screen.getByText(/Hello world/i)
-    expect(helloWorld).toBeInTheDocument();
+        const button = screen.getByRole("button")
+        userEvent.click(button)
+        //Assert
+
+        const text = screen.getByText("changed here")
+        expect(text).toBeInTheDocument()
+    })
+
+
+    test("check if text is not visible", () => {
+
+        // Arrange
+        render(<Greeting />)
+        //Act
+
+        const button = screen.getByRole("button")
+        userEvent.click(button)
+        //Assert
+
+        const text = screen.queryByText("Its good to see you")
+        expect(text).toBeNull()
+
+    })
 })
+// AAA
+
 
 
